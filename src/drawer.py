73e882d6ab31b5310dcb1b5e9963ff9b934e6a54
rnoB <rno.bstin@gmail.com>
2021-04-04 11:04:11 +0200

    small function to control line-us

---
+++ src/drawer.py
@@ -0,0 +1,83 @@
+import serial
+import time
+import math
+import numpy as np
+import random
+import sys
+import os
+import traceback
+from lineus import LineUs
+
+
+
+
+
+offset = [650,-1000]
+sizeMax = [1775-650,1000+1000]
+
+
+
+
+
+def noiser(xMax):
+    return xMax*random.random()
+
+
+
+class Drawer:
+
+    penPosition = True
+    output = False
+    penCode = [1000,0]
+    X = []
+
+
+
+
+
+    def toPosition(self,x0,y0,penPosition):
+        if x0 < sizeMax[0] and y0<sizeMax[1] and x0>=0 and y0>=0:
+            self.drawer.g01(x0+offset[0],y0+offset[1],penPosition)
+
+
+
+
+    def closeDrawer(self):
+        self.drawer.disconnect()
+
+
+
+    def line(self,x0,y0,xf=-999,yf=-999,length=1,angle=0):
+        
+        xf = x0+length*math.cos(angle)+xOffset
+        yf = y0+length*math.sin(angle)+yOffset
+        self.toPosition(x0,y0,penCode[1])
+        self.toPosition(x0,y0,penCode[0])
+        self.toPosition(xf,yf,penCode[0])
+        self.toPosition(xf,yf,penCode[1])
+        
+
+    def lines(self,x,y):
+        x0 = x[0]
+        y0 = y[0]
+        self.toPosition(x0,y0,penCode[1])
+        self.toPosition(x0,y0,penCode[0])
+
+        k0=0
+        try:
+            for k in range(0,len(x)):
+                k0 = k
+                x0 = x[k0]
+                y0 = y[k0]
+                self.toPosition(x0,y0,penCode[0])
+            self.toPosition(x0,y0,penCode[1])
+        except:
+            print('--- CRASH !!!! ---')
+            print("length : "+str(len(x)))
+            print("  k0   : "+str(k0))
+            self.toPosition(x[0],y[0],penCode[1])
+
+
+    def __init__(self,output = False,dx = 0,dy=0,de = 40):
+        self.drawer = LineUs()
+        self.drawer.connect()
